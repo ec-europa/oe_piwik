d7b95a06a2b3ad42ab9751b2d6a44a8a8758d2e9
AlbertoPerezEveris <aperedos@everis.com>
2018-01-11 14:09:18 +0100

    JS admin repair tabs message

--- js/oe_piwik.admin.js
+++ js/oe_piwik.admin.js
@@ -3,16 +3,24 @@
  * This file provides a summary to the tabs of the configuration form.
  */
 
-(function ($) {
+(function ($, window) {
+
+  "use strict";
 
   /**
    * Provide the summary information for the tracking settings vertical tabs.
    */
   Drupal.behaviors.trackingSettingsSummary = {
     attach: function (context) {
-      if ($('fieldset#edit-page-vis-settings').length > 0) {
-        if ($('input[name="oe_piwik_visibility_pages"]:checked').val() == 0) {
-          if ($('textarea[name="oe_piwik_pages"]').val() == '') {
+      // Make sure this behavior is processed only if drupalSetSummary is defined.
+      if (typeof jQuery.fn.drupalSetSummary === 'undefined') {
+        return;
+      }
+
+      $('#edit-page-vis-settings').drupalSetSummary(function (context) {
+        var $radio = $('input[name="oe_piwik_visibility_pages"]:checked', context);
+        if ($radio.val() === '0') {
+          if (!$('textarea[name="oe_piwik_pages"]', context).val()) {
             return Drupal.t('Not restricted');
           }
           else {
@@ -22,22 +30,22 @@
         else {
           return Drupal.t('Restricted to certain pages');
         }
-      }
-      if ($('fieldset#edit-role-vis-settings').length > 0) {
+      });
+      $('#edit-role-vis-settings').drupalSetSummary(function (context) {
         var vals = [];
-        $('input[type="checkbox"]:checked').each(function () {
+        $('input[type="checkbox"]:checked', context).each(function () {
           vals.push($.trim($(this).next('label').text()));
         });
-        if (vals.length < 1) {
+        if (!vals.length) {
           return Drupal.t('Not restricted');
         }
-        else if ($('input[name="oe_piwik_visibility_roles"]:checked').val() == 1) {
-          return Drupal.t('Excepted: @roles', {'@roles' : vals.join(', ')});
+        else if ($('input[name="oe_piwik_visibility_roles"]:checked', context).val() === '1') {
+          return Drupal.t('Excepted: @roles', {'@roles': vals.join(', ')});
         }
         else {
           return vals.join(', ');
         }
-      }
+      });
     }
   };
 })(jQuery);
